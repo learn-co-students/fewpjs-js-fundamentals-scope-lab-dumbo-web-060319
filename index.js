472fb8d753afa07222c5db236f449497b453e4ef
Gia Edgington <giaedgington@Erics-MacBook.local>
2019-07-15 21:14:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-animal = ""
+const myanimal = "dog";
+const youranimal = "cat";
 
 function myAnimal() {
-  return animal
+  return myanimal;
 }
 
 function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  return animal
+  return youranimal;
 }
 
 function add2(n) {
-  return n + two
-
+  let two = 2;
+  return n + two;
   // Feel free to move things around!
-  two = 2
 }
+
+
+// In index.js, we've given you some starter code. As is, it does not run. Start by running the specs and reading the results. Debug and make some fixes in the code. Run the tests again and repeat. Using your knowledge of scope, access and change the variables until all tests pass. Make sure to focus on using const and let and avoid usage of var, or you may experience some unexpected issues.
